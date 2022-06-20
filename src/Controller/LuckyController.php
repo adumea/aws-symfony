2371a5f48c950061e49c1b1d930787dfb7984dff
Alexandru Dumea <a.dumea@modigagency.com>
2022-06-20 16:16:53 +0300

    Composer config

--- src/Controller/LuckyController.php
+++ src/Controller/LuckyController.php
@@ -1,12 +1,21 @@
+
 <?php
 namespace App\Controller;
 
+use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 
-class LuckyController
+class LuckyController extends AbstractController
 {
-    public function number(): Response
+    public function number(ManagerRegistry $doctrine): Response
     {
+        $entityManager = $doctrine->getManager();
+        if ($entityManager->getConnection()->connect()) {
+            echo 'DOCTRINE WORKS';
+        } else {
+            echo 'Could not connect';
+        }
         $number = random_int(0, 100);
 
         return new Response(
